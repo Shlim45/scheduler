5688334f54af3d7877bb71f5197c2448b0b27e41
Jon Hawranko <JHawranko@gmail.com>
2022-01-30 10:23:44 -0500

    Add call to closeConnection() on System.exit

--- src/main/Main.java
+++ src/main/Main.java
@@ -18,6 +18,9 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         JDBC.openConnection();
+
         launch(args);
+
+        JDBC.closeConnection();
     }
 }
